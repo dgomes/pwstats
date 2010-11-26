c826f7a7b2dbb03c5d30fdc228eee4c7a3124668
João Paulo Barraca <jpbarraca@ua.pt>
2010-11-26 22:26:27 +0000

    Fixes for cron.php

--- system/application/controllers/cron.php
+++ system/application/controllers/cron.php
@@ -2,24 +2,28 @@
 
 class Cron extends Controller {
 
-       function Cron()
-       {
-	       parent::Controller();
-	       $this->load->helper('url');
-       }
-       function index()
-       {
-	       $this->db->select('id, models_id');
-	       $readers = $this->db->get('readers');
-	       foreach($readers->result() as $row) {
+		function Cron()
+		{
+			parent::Controller();
+			$this->load->helper('url');
+		}
+		function index()
+		{
+			$this->db->select('id, models_id');
+			$readers = $this->db->get('readers');
+
+			foreach($readers->result() as $row) {
 			   	$this->db->select('plugin');
 			   	$model = $this->db->get_where('models',array('id' => $row->models_id))->result();
 
-				$this->load->library("reader_plugins/".$model[0]->plugin);
-
-				if(method_exists($model[0]->plugin,"read"))
+				if(!method_exists(array("RP_".$model[0]->plugin,"read"))
 				{
-						$ret = call_user_func(array($model[0]->plugin,"read"),$model[0]->id);
+					$this->load->library("reader_plugins/".$model[0]->plugin, NULL,"RP_".$model[0]->plugin);
+				}
+
+				if(method_exists(array("RP_".$model[0]->plugin,"read"))
+				{
+						$ret = call_user_func(array("RP_".$model[0]->plugin,"read"),$row->id);
 				}else
 				{
 						echo "Could not load plugin: ".$model[0]->plugin."<br />";
