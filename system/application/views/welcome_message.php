b79c31e0640e4754d9bcbcd2248e8791de690a22
Diogo Gomes <diogogomes@gmail.com>
2010-11-26 11:11:49 +0000

    default welcome note

--- system/application/views/welcome_message.php
+++ system/application/views/welcome_message.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>Welcome to CodeIgniter</title>
+<title>PWStats</title>
 
 <style type="text/css">
 
@@ -43,20 +43,9 @@ code {
 </head>
 <body>
 
-<h1>Welcome to CodeIgniter!</h1>
+<h1>PWStats</h1>
 
-<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-<p>If you would like to edit this page you'll find it located at:</p>
-<code>system/application/views/welcome_message.php</code>
-
-<p>The corresponding controller for this page is found at:</p>
-<code>system/application/controllers/welcome.php</code>
-
-<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-
-
-<p><br />Page rendered in {elapsed_time} seconds</p>
+<p>Comming Soon...</p>
 
 </body>
 </html>
