1e1a26097a2dd6706a02e0583f10d0460aede2f4
Diogo Gomes <diogogomes@gmail.com>
2010-11-26 17:45:02 +0000

    Add scaffolding for readers

---
+++ system/application/controllers/readers.php
@@ -0,0 +1,12 @@
+<?php
+class Readers extends Controller {
+
+       function Readers()
+       {
+            parent::Controller();
+            $this->load->library('tank_auth');
+	    if ($this->tank_auth->is_logged_in())
+            	$this->load->scaffolding('readers');
+       }
+}
+?>
