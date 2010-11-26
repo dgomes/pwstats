509de4d284fb73051bfc372dc581d02bdbb90971
Diogo Gomes <diogogomes@gmail.com>
2010-11-26 15:05:41 +0000

    fix to welcome message

--- system/application/controllers/welcome.php
+++ system/application/controllers/welcome.php
@@ -13,7 +13,7 @@ class Welcome extends Controller
 	function index()
 	{
 		if (!$this->tank_auth->is_logged_in()) {
-			$this->load->view('welcome_message', $data);
+			$this->load->view('welcome_message');
 		} else {
 			$data['user_id']	= $this->tank_auth->get_user_id();
 			$data['username']	= $this->tank_auth->get_username();
