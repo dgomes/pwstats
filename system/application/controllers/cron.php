8b4be175d52373978dc11e897e87893b71fb8a5f
João Paulo Barraca <jpbarraca@ua.pt>
2010-11-26 21:51:57 +0000

    Converted plugin to library

--- system/application/controllers/cron.php
+++ system/application/controllers/cron.php
@@ -1,4 +1,5 @@
-<?php
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
 class Cron extends Controller {
 
        function Cron()
@@ -11,22 +12,22 @@ class Cron extends Controller {
 	       $this->db->select('id, models_id');
 	       $readers = $this->db->get('readers');
 	       foreach($readers->result() as $row) {
-		       $this->db->select('plugin');
-			   $model = $this->db->get_where('models',array('id' => $row->models_id))->result();
+			   	$this->db->select('plugin');
+			   	$model = $this->db->get_where('models',array('id' => $row->models_id))->result();
 
-			   if(file_exists("system/application/controllers/plugins/"+$model[0]->plugin+".php"))
-			   {
-				   include_once("system/applications/controllers/plugins"+$model[0]->plugin+".php");
-				   if(function_exists($model[0]->plugin+".read"))
-				   {
+				if(!function_exists($model[0]->plugin+".read"))
+				{
+					$this->load->library("reader_plugins/".$model[0]->plugin);
+			   	}
+
+				if(function_exists($model[0]->plugin+".read"))
+				{
 						$ret = call_user_func($model[0]->plugin+".read",$model[0]->id);
-				   }else
-				   {
-						echo "Could not load plugin<br />";
-				   }
-			   }else{
-					echo "Could not find plugin: ".$model[0]->plugin."<br />";
-			   }
+				}else
+				{
+						echo "Could not load plugin: ".$model[0]->plugin."<br />";
+				}
+
 			}
        }
 }
---
+++ system/application/libraries/reader_plugins/imeterbox.php
@@ -0,0 +1,50 @@
+<?php
+class Imeterbox {
+
+	function index() {
+		echo "ISA iMeterBor plugin";
+	}
+
+	function read($readers_id) {
+		$reader = $this->db->get_where('readers', array('id' => $readers_id))->result();
+		$data = $this->_parse_imeter($reader[0]->url);
+		$query = $this->db->get('entries_types');
+		$type = array();
+		foreach ($query->result() as $row)
+			$type[$row->name] = $row->id;
+
+		foreach($data['measure'] as $val_type => $val)
+			if(array_key_exists($val_type,$type)) {
+				$this->db->set('readers_id', (int) $readers_id);
+				$this->db->set('entry', $val);
+				$this->db->set('entries_types_id', $type[$val_type]);
+				$this->db->set('unit_timestamp', $data['timestamp']);
+				$this->db->insert('entries');
+			}
+	}
+
+	function _parse_imeter($uri) {
+		$dom = new DOMDocument;
+		$dom->loadHTMLFile($uri);
+		$s = simplexml_import_dom($dom);
+
+		$measure = $s->body->div->div[2]->table[1]->tbody->tr[2]->td[2];
+		$measure = explode("\n",$measure);
+		array_walk($measure, function(&$s, &$key) {
+			$s = trim($s);
+			$t = explode(" ", $s);
+			$s = $t[0];
+		});
+		$measure['Energy'] = $measure[1];
+		$measure['Power'] = $measure[2];
+		$measure['Current'] = $measure[3];
+		$measure['Voltage'] = $measure[4];
+
+		$time = $s->body->div->div[2]->table[1]->tbody->tr[3]->td[1];
+		$timestamp = str_replace(":","*",$time); //in accordance with http://dev.mysql.com/doc/refman/5.0/en/datetime.html formats '98/12/31 11*30*45'
+
+		return array('measure' => $measure, 'timestamp' => $timestamp);
+	}
+
+}
+?>
