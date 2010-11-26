a062091da116c1cc92a34f0efacd039b980dedc5
Diogo Gomes <diogogomes@gmail.com>
2010-11-26 19:59:13 +0000

    list entries

---
+++ system/application/controllers/series.php
@@ -0,0 +1,18 @@
+<?php
+class Series extends Controller {
+
+       function Series()
+       {
+	       parent::Controller();
+	       $this->load->helper('url');
+       }
+       function lastday($reader_id)
+       {
+		$this->db->select("entry, UNIX_TIMESTAMP(unit_timestamp) AS ts FROM entries WHERE entry_types_id = 2", FALSE);
+	       $val = $this->db->get(); 
+		foreach($val->result() as $row) {
+			echo json_encode($row).",";
+	       }
+       }
+}
+?>
