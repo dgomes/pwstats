3758c8a3650deba10028042b170e34c1b5195d98
João Paulo Barraca <jpbarraca@ua.pt>
2010-11-27 15:37:10 +0000

    Added test code for user charts

--- system/application/controllers/charts.php
+++ system/application/controllers/charts.php
@@ -14,10 +14,11 @@ class Charts extends Controller
 	{
 		if (!$this->tank_auth->is_logged_in()) {
 			redirect('/auth/login', 'refresh');
+			return;
 		} else {
 			$data['user_id']	= $this->tank_auth->get_user_id();
-			$data['username']	= $this->tank_auth->get_username();
-			$this->load->view('template', 'user_charts');
+
+			$this->load->view('template', 'user_charts', $data);
 		}
 	}
 }
--- system/application/views/user_charts.php
+++ system/application/views/user_charts.php
@@ -1,8 +1,13 @@
-<html>
-<head>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/highcharts.js"></script>
+<script type="text/javascript" src="js/exporting.js"></script>
+
+<script type="text/javascript">
+	var $user_id = <?= $user_id ?>;
+</script>
+
+<script type="text/javascript" src="js/views/user_charts.js"></script>
 
 
-</body>
+<div id="container" class="highcharts-container" style="height:100%; margin: 0 2em; clear:both; min-width: 400px">
 
-</body>
-</html>
