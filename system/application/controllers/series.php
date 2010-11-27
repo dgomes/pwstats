cd2ec0ad3102699a99d2e1f36c6511bd23898b3b
Diogo Gomes <diogogomes@gmail.com>
2010-11-27 00:35:51 +0000

    test

--- system/application/controllers/series.php
+++ system/application/controllers/series.php
@@ -8,7 +8,8 @@ class Series extends Controller {
        }
        function lastday($reader_id)
        {
-	       $this->db->select("entry AS power, UNIX_TIMESTAMP(unit_timestamp) AS ts FROM entries,entries_types WHERE entries_types.name='Power' AND entries.entries_types_id = entries_types.id", FALSE);
+	       //$this->db->select("entry AS power, UNIX_TIMESTAMP(unit_timestamp) AS ts FROM entries,entries_types WHERE entries_types.name='Power' AND entries.entries_types_id = entries_types.id", FALSE);
+	       $this->db->select("entry AS power, UNIX_TIMESTAMP(unit_timestamp) AS ts FROM entries,entries_types WHERE AND entries.reader_id = $reader_id", FALSE);
 	       $val = $this->db->get(); 
 		foreach($val->result() as $row) {
 			echo json_encode($row).",";
