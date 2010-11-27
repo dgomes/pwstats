9550eb8f91d8aef9e76a4563cb9c63eaebbb1190
jpbarraca <jpbarraca@ua.pt>
2010-11-27 13:39:19 +0000

    Preliminary template style

--- system/application/views/menu.php
+++ system/application/views/menu.php
@@ -1,14 +1,15 @@
-<ul>
-	<li><a href="">Home</a></li>
-	<li><a href="charts">Charts</a></li>
-	<li><a href="simulate">Simulate</a></li>
-	<li><a href="devices">Devices</a></li>
-	<?php
-		if(isset($user_id))
-		{
-	?>
-			<li><a href="auth/logout">Logout</a></li>
-	<?php 	}else{ ?>
-			<li><a href="auth/login">Login</a></li>
-	<?php  	} ?>
-</ul>
+	<ul>
+		<li><a href="">Home</a></li>
+		<li><a href="charts">Charts</a></li>
+		<li><a href="simulate">Simulate</a></li>
+		<li><a href="devices">Devices</a></li>
+		<?php
+			if(isset($user_id))
+			{
+		?>
+				<li><a href="auth/logout">Logout</a></li>
+		<?php 	}else{ ?>
+				<li><a href="auth/login">Login</a></li>
+		<?php  	} ?>
+	</ul>
+	<div class="cleaner"></div> 	
--- system/application/views/template.php
+++ system/application/views/template.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <html>
 
 	<head>
@@ -7,27 +9,55 @@
 	</head>
 
 <body>
-	<div id="menu">
-		<?php include_once("menu.php") ?>
+	<div id="header">
+		<div id="logo"></div>
+		<div id="menu">
+			<?php include_once("menu.php") ?>
+		</div>
+		 <div class="cleaner"></div>
 	</div>
-	<div id="alert">
-		<?php if(isset($alert)) echo $alert; ?>
-	</div>
+	<div id="banner_wrapper">
+		<div id="banner">
+			<div id="banner_content">
+			<div id="banner_title">Vivamus a velit sociis natoque penatibus</div>
+			<div id="banner_text">
+			</div>
+	    
+			<div class="cleaner"></div>
+			</div> <!-- end of banner content -->
+			<div class="cleaner"></div>   
+		</div> <!-- end of banner -->
+	</div> <!-- end of banner wrapper -->
 
-	<div id="content">
-		<?php if(isset($content)) echo $content; ?>
-	</div>
+	<div id="content_top_wrapper">
+		<div id="content_top">
+    
+			<div class="header_01">Alerts</div>
+			<div id="alert">
+				<?php if(isset($alert)) echo $alert; ?>
+			</div>
+			<div class="margin_bottom_10"></div>
+			<div class="cleaner"></div>
+	    	</div> <!-- end of content top -->
+	</div> <!-- end of content top wrapper -->
 
-	<div id="left_panel">
-		<?php if(isset($left_panel)) echo $left_panel; ?>
-	</div>
+	<div id="content_wrapper">
+		<div id="content">
+			<?php if(isset($content)) echo $content; ?>
+		</div>
 
-	<div id="right_panel">
-		<?php if(isset($right_panel)) echo $right_panel; ?>
-	</div>
+		<div id="left_panel">
+			<?php if(isset($left_panel)) echo $left_panel; ?>
+		</div>
 
-	<div id="footer">
-		<?php include_once("footer.php") ?>
+		<div id="right_panel">
+			<?php if(isset($right_panel)) echo $right_panel; ?>
+		</div>
+	</div>
+	<div id="footer_wrapper">
+    		<div id="footer">
+			<?php include_once("footer.php") ?>
+		</div>
 	</div>
 </body>
 </html>
