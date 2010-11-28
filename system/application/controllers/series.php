abd18f86ad423d633480b6dcb9a724b87d232904
João Paulo Barraca <jpbarraca@ua.pt>
2010-11-28 12:21:56 +0000

    Optimized series controller to reduce output size

--- system/application/controllers/series.php
+++ system/application/controllers/series.php
@@ -8,13 +8,13 @@ class Series extends Controller {
        }
        function lastday($reader_id)
        {
-	       echo "[";
 	       $this->db->select("entries.entry AS power, UNIX_TIMESTAMP( entries.unit_timestamp ) AS ts FROM entries, entries_types WHERE entries.readers_id = $reader_id AND  entries_types.name='Power' AND entries.entries_types_id = entries_types.id", FALSE);
-	       $val = $this->db->get(); 
-		foreach($val->result() as $row) {
-			echo json_encode($row).",";
-	       }
-	       echo "]";
+		   $val = $this->db->get();
+		   $values= null;
+		   foreach($val->result() as $row) {
+			   $values.= array( 'x' => $row['ts'], 'y'=> $row['power']);
+		   }
+		  	echo  "[ 'name': 'Power', 'values': ".json_encode($values)."]";
        }
 }
 ?>
