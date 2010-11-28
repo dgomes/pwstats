0afb6fcb1cb813b22ac30a95d3b9fc697f79f386
João Paulo Barraca <jpbarraca@ua.pt>
2010-11-28 11:56:23 +0000

    Fixed user_charts_head

--- system/application/views/user_charts_head.php
+++ system/application/views/user_charts_head.php
@@ -1,2 +1,9 @@
-<div id="container" class="highcharts-container" style="height:100%; margin: 0 2em; clear:both; min-width: 400px">
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/highcharts.js"></script>
+<script type="text/javascript" src="js/exporting.js"></script>
 
+<script type="text/javascript">
+	var $user_id = <?= $user_id ?>;
+</script>
+
+<script type="text/javascript" src="js/views/user_charts.js"></script>
