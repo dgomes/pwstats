ab50122b00bdd457b60777112bdfa6c0a38c0d42
jpbarraca <jpbarraca@ua.pt>
2010-11-28 18:08:09 +0000

    Fixed series output

--- system/application/controllers/series.php
+++ system/application/controllers/series.php
@@ -8,13 +8,14 @@ class Series extends Controller {
        }
        function lastday($reader_id)
        {
-	       $this->db->select("entries.entry AS power, UNIX_TIMESTAMP( entries.unit_timestamp ) AS ts FROM entries, entries_types WHERE entries.readers_id = $reader_id AND  entries_types.name='Power' AND entries.entries_types_id = entries_types.id", FALSE);
-		   $val = $this->db->get();
-		   $values= null;
-		   foreach($val->result() as $row) {
-			   $values.= array( 'x' => $row['ts'], 'y'=> $row['power']);
-		   }
-		  	echo  "[ 'name': 'Power', 'values': ".json_encode($values)."]";
+		$reader_id = (int) $reader_id;
+		$this->db->select("entries.entry AS power, UNIX_TIMESTAMP( entries.unit_timestamp ) AS ts FROM entries, entries_types WHERE entries.readers_id = $reader_id AND  entries_types.name='Power' AND entries.entries_types_id = entries_types.id", FALSE);
+		$val = $this->db->get();
+		$data = array();
+		foreach($val->result() as $row) {
+			$data[] = array( (int) $row->ts, (int) $row->power);
+		}
+		echo  "[ 'name': 'Power', 'values': ".json_encode($data)."]";
        }
 }
 ?>
