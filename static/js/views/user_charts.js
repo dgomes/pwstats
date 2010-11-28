1ceb9df3a123c32b24baf341d05153e34f6251c0
jpbarraca <jpbarraca@ua.pt>
2010-11-28 19:02:54 +0000

    Data can be extracted and draw based on age

--- static/js/views/user_charts.js
+++ static/js/views/user_charts.js
@@ -2,21 +2,26 @@
 Highcharts.theme = {};// prevent errors in default theme
 var highchartsOptions = Highcharts.getOptions();
  
-var dataUrl = base_url+"/series/lastday/"+reader_id;
+var dataUrl = base_url+"/series/reader/"+reader_id;
 var maskMin = 0;
 var maskMax = 0;
 var min = 0;
 var max = 0;
+var time = 86400
  
 var masterChart,detailChart;
  
 $(document).ready(function() {
-    var $container = $('#container').css('position', 'relative');
-    var $detailContainer = $('<div id="detail-container">').appendTo($container);
-    var $masterContainer = $('<div id="master-container">').css({ position: 'absolute', top: 510, height: 80, width: '100%' }).appendTo($container);
-    createMaster();
- 
-    $.get(dataUrl, function(resp) {
+	var $container = $('#container').css('position', 'relative');
+	var $detailContainer = $('<div id="detail-container">').appendTo($container);
+	var $masterContainer = $('<div id="master-container">').css({ position: 'absolute', top: 510, height: 80, width: '100%' }).appendTo($container);
+	createMaster();
+	load_data(time);
+});
+
+function load_data(ts)
+{
+    $.get(dataUrl+"/"+ts, function(resp) {
         var data = eval("("+resp+")");
         jQuery.each(data.values, function(i, value) {
             data.values[i][0] = data.values[i][0] * 1000;
@@ -27,7 +32,18 @@ $(document).ready(function() {
         detailChart.redraw();
  
     });
-});
+}
+
+function reload_chart(ts)
+{
+        jQuery.each(masterChart.series, function(i, series) {
+        	series.remove(false);
+	});
+        jQuery.each(detailChart.series, function(i, series) {
+        	series.remove(false);
+	});
+	load_data(ts);
+}
  
 function addSeries(values,sname)
 {
--- system/application/views/user_charts.php
+++ system/application/views/user_charts.php
@@ -1,2 +1,10 @@
+<div>
+  <input type="button" value="Day" onClick="reload_chart(86400);" />
+  <input type="button" value="Week" onClick="reload_chart(604800);" />
+  <input type="button" value="Month" onClick="reload_chart(2592000);" />
+  <input type="button" value="Year" onClick="reload_chart(31104000);" />
+  <input type="button" value="All" onClick="reload_chart(0);" />
+</div>
+
 <div id="container" class="highcharts-container" style="height:600px; margin: 0 2em; clear:both; min-width: 400px"></div>
 
