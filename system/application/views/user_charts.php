ac7636373fb03b0be34a56039aa78dc10620d7e1
João Paulo Barraca <jpbarraca@ua.pt>
2010-11-28 12:11:58 +0000

    Set charts height

--- system/application/views/user_charts.php
+++ system/application/views/user_charts.php
@@ -1,2 +1,2 @@
-<div id="container" class="highcharts-container" style="height:100%; margin: 0 2em; clear:both; min-width: 400px"></div>
+<div id="container" class="highcharts-container" style="height:600px; margin: 0 2em; clear:both; min-width: 400px"></div>
 
