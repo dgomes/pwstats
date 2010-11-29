23deaa75ee384ad252ddfddfe3a7f8e301c64dcd
João Paulo Barraca <jpbarraca@ua.pt>
2010-11-29 23:29:53 +0000

    Added missing space character to sql

--- system/application/controllers/series.php
+++ system/application/controllers/series.php
@@ -15,7 +15,7 @@ class Series extends Controller {
 
 		function reader($reader_id, $age = 86400 )
 		{
-			 if (!$this->tank_auth->is_logged_in()) 
+			 if (!$this->tank_auth->is_logged_in())
 			 {
 				 return;
 			 }
@@ -28,8 +28,8 @@ class Series extends Controller {
 				$date->setTimestamp(0);
 			else
 				$date->setTimestamp($start);
-			$this->db->select("entries.entry AS power,". 
-				"UNIX_TIMESTAMP( entries.unit_timestamp ) AS ts FROM entries, entries_types".
+			$this->db->select("entries.entry AS power,".
+				"UNIX_TIMESTAMP( entries.unit_timestamp ) AS ts FROM entries, entries_types ".
 				"WHERE entries.readers_id = $reader_id AND  entries_types.name='Power' ".
 				"AND entries.entries_types_id = entries_types.id ".
 				"AND UNIX_TIMESTAMP(entries.site_timestamp) > ".$date->getTimestamp(), FALSE);
