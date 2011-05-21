d1d9abcd4fe8dd45c401765086dd7b3f3450e3b5
João Paulo Barraca <jpbarraca@av.it.pt>
2011-05-21 16:11:05 +0100

    Menu item visibility is conditioned by auth status

--- system/application/views/menu.php
+++ system/application/views/menu.php
@@ -1,14 +1,12 @@
 <ul id="menu">
-	<li><a href="">Home</a></li>
+<li><a href="<?=base_url()?>">Home</a></li>
+	<? if(isset($user_id)){ ?>
 	<li><a href="<?=base_url()?>charts">Charts</a></li>
 	<li><a href="<?=base_url()?>simulate">Simulate</a></li>
 	<li><a href="<?=base_url()?>devices">Devices</a></li>
-	<?php
-		if(isset($user_id))
-		{
-	?>
-		<li><a href="<?=base_url()?>auth/logout">Logout</a></li>
-	<?php 	}else{ ?>
-	<li><a href="<?=base_url()?>auth/login">Login</a></li>
+	<li><a href="<?=base_url()?>auth/logout">Logout</a></li>
+	<?php 	
+	}else{ ?>
+		<li><a href="<?=base_url()?>auth/login">Login</a></li>
 	<?php  	} ?>
 </ul>
